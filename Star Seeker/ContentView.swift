90d67bd7c54f2b3fdf1c1ffe5de23f7871bd0575
KurooNacht <125423505+KurooNacht@users.noreply.github.com>
2024-06-10 08:45:14 +0700

    adding character animation (unfinished)

--- Star Seeker/ContentView.swift
+++ Star Seeker/ContentView.swift
@@ -3,24 +3,40 @@ import SwiftData
 import SwiftUI
 
 struct ContentView : View {
-
+    
     let sw = UIScreen.main.bounds.width
     let sh = UIScreen.main.bounds.height
     
     var scene : SKScene {
         let s = Game()
-//        s.size = CGSize(width: sw, height: sh)
+        //        s.size = CGSize(width: sw, height: sh)
         s.scaleMode = .resizeFill
         
-        let image = SKSpriteNode(color: .red, size: CGSize(width: sw/9, height: sh/19.5))
-        image.position = CGPoint(x: sw/2, y: sh/2)
-        image.physicsBody = SKPhysicsBody(rectangleOf: image.size)
-        image.physicsBody?.isDynamic = true
-        image.physicsBody?.mass = 0.25
-        image.physicsBody?.linearDamping = 1
-        image.physicsBody?.friction = 0.6
-        s.addChild(image)
-
+        let firstFrame = SKTexture(imageNamed: "walk1")
+        let player = SKSpriteNode(texture: firstFrame)
+        player.size = CGSize(width: sw / 9, height: sh / 19.5)
+        player.position = CGPoint(x: sw / 2, y: sh / 2)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.mass = 0.25
+        player.physicsBody?.linearDamping = 1
+        player.physicsBody?.friction = 0.6
+        player.physicsBody?.allowsRotation = false
+        s.addChild(player)
+        
+        // Animation setup
+        var walkTextures: [SKTexture] = []
+        for i in 1...20 {
+            let textureName = "walk\(i)"
+            let texture = SKTexture(imageNamed: textureName)
+            walkTextures.append(texture)
+        }
+        
+        let walkAnimation = SKAction.animate(with: walkTextures, timePerFrame: 0.03)
+        let repeatAnimation = SKAction.repeatForever(walkAnimation)
+        
+        player.run(repeatAnimation)
+        
         let platform = SKSpriteNode(color: .green, size: CGSize(width: 3000, height: 6*(sh/19.5)))
         platform.position = CGPoint(x: 0, y: 0)
         platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
@@ -29,12 +45,12 @@ struct ContentView : View {
         platform.physicsBody?.friction = 0.2
         s.addChild(platform)
         
-//        let floatingPlatform = SKSpriteNode(color: .green, size: CGSize(width: 250, height: (sh/19.5)))
-//        floatingPlatform.position = CGPoint(x: 0, y: 3*(sh/19.5))
-//        floatingPlatform.physicsBody = SKPhysicsBody(rectangleOf: floatingPlatform.size)
-//        floatingPlatform.physicsBody?.isDynamic = false
-//        floatingPlatform.physicsBody?.friction = 0.3
-//        s.addChild(floatingPlatform)
+        //        let floatingPlatform = SKSpriteNode(color: .green, size: CGSize(width: 250, height: (sh/19.5)))
+        //        floatingPlatform.position = CGPoint(x: 0, y: 3*(sh/19.5))
+        //        floatingPlatform.physicsBody = SKPhysicsBody(rectangleOf: floatingPlatform.size)
+        //        floatingPlatform.physicsBody?.isDynamic = false
+        //        floatingPlatform.physicsBody?.friction = 0.3
+        //        s.addChild(floatingPlatform)
         
         let floatingPlatform2 = SKSpriteNode(color: .cyan, size: CGSize(width: 250, height: (sh/19.5)))
         floatingPlatform2.position = CGPoint(x: 250, y: 250)
@@ -50,7 +66,7 @@ struct ContentView : View {
         floatingPlatform3.physicsBody?.friction = GameConfig.stickyFrictionModifier
         s.addChild(floatingPlatform3)
         
-        let movementController = MovementController(target: image)
+        let movementController = MovementController(target: player)
         movementController.position = CGPoint(x: UIConfig.Paddings.huge, y: UIConfig.Paddings.huge * 2)
         s.addChild(movementController)
         
@@ -65,8 +81,8 @@ struct ContentView : View {
             SpriteView(scene: scene, options: [.allowsTransparency])
                 .ignoresSafeArea(.all)
                 .background(.clear)
-//                .frame(width: 1.25*(sw/2), height: 1.25*(sh/2))
-//                .padding(.bottom, UIConfig.Paddings.huge)
+            //                .frame(width: 1.25*(sw/2), height: 1.25*(sh/2))
+            //                .padding(.bottom, UIConfig.Paddings.huge)
             VStack (spacing: 0) {
                 gridRow
                 gridRow
